6e0f8ee717abb42009cfadd5697a18a9dbd669e5
Administrator <931317632@qq.com>
2017-08-30 09:39:21 +0800

    20170830

---
+++ app/src/main/java/com/likeit/a51scholarship/adapters/SchoolDetailsAdapter.java
@@ -0,0 +1,61 @@
+package com.likeit.a51scholarship.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.likeit.a51scholarship.R;
+import com.likeit.a51scholarship.model.SchoolDetailsBean01;
+import com.likeit.a51scholarship.utils.richtext.RichText;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/8/26.
+ */
+
+public class SchoolDetailsAdapter extends MyBaseAdapter<SchoolDetailsBean01> {
+    private int selectItem = -1;
+    public SchoolDetailsAdapter(Context context, List<SchoolDetailsBean01> schoolDetailsBean01s) {
+        super(context, schoolDetailsBean01s);
+    }
+    public void setSelectItem(int selectItem) {
+        this.selectItem = selectItem;
+    }
+    @Override
+    public View getItemView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = getInflater().inflate(
+                    R.layout.shool_details_listview_items, parent, false);
+            holder.schoolDetailsName = (TextView) convertView
+                    .findViewById(R.id.school_details_name);
+            holder.schoolDetails = (RichText) convertView
+                    .findViewById(R.id.school_details_details);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        SchoolDetailsBean01 data1 = getItem(position);
+        holder.schoolDetailsName.setText(data1.getName());
+        holder.schoolDetails.setRichText(data1.getContent());
+//        holder.schoolDetails.setOnRichTextImageClickListener(new RichText.OnRichTextImageClickListener(){
+//            @Override
+//            public void imageClicked(List<String> imageUrls, int position){
+//                Toast.makeText(context, imageUrls.get(position),Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView schoolDetailsName;
+        RichText schoolDetails;
+
+    }
+
+
+}
